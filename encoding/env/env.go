65ff2fc6ea14b9fef796b604cd570c553167a960
Ryan Ball <ryanlball@gmail.com>
2019-02-17 15:30:14 -0700

    encoding interfaces

---
+++ encoding/env/env.go
@@ -0,0 +1,10 @@
+package env
+
+// Load will load env variables into the provided struct pointer.
+// If there is an issue with loading then an error is returned.
+func Load(v interface{}) error {
+	// get a list of expected env variables.
+
+
+	return nil
+}
