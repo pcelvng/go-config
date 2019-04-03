ad5c6e74f66a75cb3284e772b92aacb26a17c738
Joshua Smith <joshua.smith@rakuten.com>
2019-04-03 15:41:48 -0600

    flag: add struct, time.duration and pointer support

--- encoding/flag/flag_test.go
+++ encoding/flag/flag_test.go
@@ -2,6 +2,7 @@ package flag
 
 import (
 	"testing"
+	"time"
 
 	"github.com/jbsmith7741/trial"
 )
@@ -12,6 +13,9 @@ func TestNew(t *testing.T) {
 		Usage string
 		Def   string
 	}
+	type Aint int
+	type Astring string
+	type AFloat64 float64
 	fn := func(args ...interface{}) (interface{}, error) {
 		f, err := New(args[0])
 		if err != nil {
@@ -44,20 +48,20 @@ func TestNew(t *testing.T) {
 				Int64: 5,
 			},
 			Expected: map[string]*tFlag{
-				"Int":   {Def: "1"},
-				"Int8":  {Def: "2"},
-				"Int16": {Def: "3"},
-				"Int32": {Def: "4"},
-				"Int64": {Def: "5"},
+				"int":    {Def: "1"},
+				"int-8":  {Def: "2"},
+				"int-16": {Def: "3"},
+				"int-32": {Def: "4"},
+				"int-64": {Def: "5"},
 			},
 		},
 		"uints": {
 			Input: &struct {
-				Uint   int
-				Uint8  int8
-				Uint16 int16
-				Uint32 int32
-				Uint64 int64
+				Uint   uint
+				Uint8  uint8
+				Uint16 uint16
+				Uint32 uint32
+				Uint64 uint64
 			}{
 				Uint:   6,
 				Uint8:  7,
@@ -66,11 +70,11 @@ func TestNew(t *testing.T) {
 				Uint64: 10,
 			},
 			Expected: map[string]*tFlag{
-				"Uint":   {Def: "6"},
-				"Uint8":  {Def: "7"},
-				"Uint16": {Def: "8"},
-				"Uint32": {Def: "9"},
-				"Uint64": {Def: "10"},
+				"uint":    {Def: "6"},
+				"uint-8":  {Def: "7"},
+				"uint-16": {Def: "8"},
+				"uint-32": {Def: "9"},
+				"uint-64": {Def: "10"},
 			},
 		},
 		"floats": {
@@ -82,22 +86,98 @@ func TestNew(t *testing.T) {
 				Float64: 2.2,
 			},
 			Expected: map[string]*tFlag{
-				"Float32": {Def: "1"},
-				"Float64": {Def: "2.2"},
+				"float-32": {Def: "1"},
+				"float-64": {Def: "2.2"},
 			},
 		},
 		"bool": {
 			Input: &struct{ Bool bool }{Bool: true},
 			Expected: map[string]*tFlag{
-				"Bool": {Def: "true"},
+				"bool": {Def: "true"},
 			},
 		},
 		"string": {
 			Input: &struct{ String string }{String: "Hello"},
 			Expected: map[string]*tFlag{
-				"String": {Def: "Hello"},
+				"string": {Def: "Hello"},
+			},
+		},
+		"with tags": {
+			Input: &struct {
+				Int  int    `flag:"Count" comment:"number of people in a room"`
+				Name string `flag:"-" comment:"ignore me"`
+			}{
+				Int:  10,
+				Name: "Bob",
+			},
+			Expected: map[string]*tFlag{
+				"Count": {Def: "10", Usage: "number of people in a room"},
+			},
+		},
+		"time": {
+			Input: &struct {
+				Time     time.Time
+				CTime    time.Time `flag:"ctime" fmt:"2006-01-02"`
+				WaitTime time.Duration
+			}{
+				Time:     trial.TimeDay("2019-01-02"),
+				CTime:    trial.TimeDay("2019-01-02"),
+				WaitTime: time.Hour,
+			},
+			Expected: map[string]*tFlag{
+				"time":      {Def: "2019-01-02T00:00:00Z"},
+				"ctime":     {Def: "2019-01-02"},
+				"wait-time": {Def: "1h0m0s"},
+			},
+		},
+		"text marshaler": {
+			Input: &struct {
+				MyStruct marshalStruct
+				IStruct  marshalStruct `flag:"-"`
+			}{
+				MyStruct: marshalStruct{value: "a"},
+				IStruct:  marshalStruct{value: "b"},
+			},
+			Expected: map[string]*tFlag{
+				"my-struct": {Def: "a"},
+			},
+		},
+		/* "alias no marshaler": {
+			Input: &struct{ Int Aint }{Int: Aint(3)},
+			Expected: map[string]*tFlag{
+				"int": {Def: "3"},
+			},
+		}, */
+		"pointers": {
+			Input: &struct {
+				Int *int
+				//	Uint     *uint
+				String   *string
+				MyStruct *marshalStruct
+			}{
+				Int:      trial.IntP(1),
+				String:   trial.StringP("a"),
+				MyStruct: &marshalStruct{value: "c"},
+			},
+			Expected: map[string]*tFlag{
+				"int":       {Def: "1"},
+				"string":    {Def: "a"},
+				"my-struct": {Def: "c"},
 			},
 		},
 	}
-	trial.New(fn, cases).Test(t)
+	trial.New(fn, cases).SubTest(t)
+}
+
+type marshalStruct struct {
+	value string
+}
+
+func (m marshalStruct) MarshalText() ([]byte, error) {
+	return []byte(m.value), nil
+}
+
+func (m *marshalStruct) UnmarshalText(b []byte) error {
+	m.value = string(b)
+	return nil
 }
