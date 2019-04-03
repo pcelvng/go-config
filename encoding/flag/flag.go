b8793542c6705641c04c91280dc276d8b97728c5
Joshua Smith <jbsmith7741@gmail.com>
2019-04-03 15:41:48 -0600

    flag: add struct, time.duration and pointer support

--- encoding/flag/flag.go
+++ encoding/flag/flag.go
@@ -1,13 +1,20 @@
 package flag
 
 import (
+	"encoding"
 	"flag"
+	"fmt"
 	"reflect"
+	"strings"
+	"time"
+
+	"github.com/iancoleman/strcase"
 )
 
 const (
 	flagTag = "flag"
 	descTag = "comment" // do we want a different tag for the flag vs toml?
+	fmtTag  = "fmt"
 )
 
 type Flags struct {
@@ -27,7 +34,7 @@ func New(i interface{}) (*Flags, error) {
 		field := vStruct.Field(i)
 		dField := vStruct.Type().Field(i)
 		tag := dField.Tag.Get(flagTag)
-		name := dField.Name
+		name := strcase.ToKebab(dField.Name)
 		desc := dField.Tag.Get(descTag)
 		if tag != "" {
 			name = tag
@@ -38,6 +45,19 @@ func New(i interface{}) (*Flags, error) {
 			continue
 		}
 
+		if isAlias(field) {
+			if field.Type().String() == "time.Duration" {
+				d := field.Interface().(time.Duration)
+				f.values[name] = f.flagSet.String(name, d.String(), desc)
+				continue
+			}
+			if implementsMarshaler(field) {
+				b, _ := field.Interface().(encoding.TextMarshaler).MarshalText()
+				f.values[name] = f.flagSet.String(name, string(b), desc)
+				continue
+			}
+		}
+	switchStart:
 		switch field.Kind() {
 		// explicit list of unsupported types
 		case reflect.Array, reflect.Func, reflect.Chan, reflect.Complex64, reflect.Complex128, reflect.Interface, reflect.Map, reflect.Slice:
@@ -72,17 +92,79 @@ func New(i interface{}) (*Flags, error) {
 		case reflect.Float64:
 			f.values[name] = f.flagSet.Float64(name, field.Interface().(float64), desc)
 		case reflect.Ptr:
-			// todo deference and handle. use recursition
+			// todo dereference and handle. use recursion
+			field = field.Elem()
+			goto switchStart // easiest solution, but do we want a goto statement
 		case reflect.Struct:
-			// todo check if struct implements TextMarshaler
-			// check if struct is time.Duration or time.Time
+			// todo: Should we add a prefix for flags? structName-childVar or only
+			// support a struct if they implement a marshaler
+			if field.Type().String() == "time.Time" {
+				timeFmt := dField.Tag.Get(fmtTag)
+				timeFmt = getTimeFormat(timeFmt)
+				t := field.Interface().(time.Time)
+				f.values[name] = f.flagSet.String(name, t.Format(timeFmt), desc)
+				continue
+			}
+			if implementsMarshaler(field) {
+				b, _ := field.Interface().(encoding.TextMarshaler).MarshalText()
+				f.values[name] = f.flagSet.String(name, string(b), desc)
+			}
 		}
 	}
 	return f, nil
 }
 
-// ToFlagName converts variable names to the flag syntax.
-// names are split based on Capitals and numbers and separated with a dash -
-func ToFlagName(s string) string {
-	return s
+func isAlias(v reflect.Value) bool {
+	if v.Kind() == reflect.Struct || v.Kind() == reflect.Ptr {
+		return false
+	}
+	s := fmt.Sprint(v.Type())
+	return strings.Contains(s, ".")
+}
+
+func implementsUnmarshaler(v reflect.Value) bool {
+	return v.Type().Implements(reflect.TypeOf((*encoding.TextUnmarshaler)(nil)).Elem())
+}
+
+func implementsMarshaler(v reflect.Value) bool {
+	return v.Type().Implements(reflect.TypeOf((*encoding.TextMarshaler)(nil)).Elem())
+}
+
+func getTimeFormat(timeFmt string) string {
+	if timeFmt == "" {
+		timeFmt = time.RFC3339 // default format
+	}
+	switch timeFmt {
+	case "ANSIC":
+		timeFmt = time.ANSIC
+	case "UnixDate":
+		timeFmt = time.UnixDate
+	case "RubyDate":
+		timeFmt = time.RubyDate
+	case "RFC822":
+		timeFmt = time.RFC822
+	case "RFC822Z":
+		timeFmt = time.RFC822Z
+	case "RFC850":
+		timeFmt = time.RFC850
+	case "RFC1123":
+		timeFmt = time.RFC1123
+	case "RFC1123Z":
+		timeFmt = time.RFC1123Z
+	case "RFC3339":
+		timeFmt = time.RFC3339
+	case "RFC3339Nano":
+		timeFmt = time.RFC3339Nano
+	case "Kitchen":
+		timeFmt = time.Kitchen
+	case "Stamp":
+		timeFmt = time.Stamp
+	case "StampMilli":
+		timeFmt = time.StampMilli
+	case "StampMicro":
+		timeFmt = time.StampMicro
+	case "StampNano":
+		timeFmt = time.StampNano
+	}
+	return timeFmt
 }
